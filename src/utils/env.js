10f97b7b007f364cf0dd031e7e53ec55d8dc10e8
jason <hainguyentran14@gmail.com>
2024-02-16 18:52:43 +0700

    update

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,4 +1,4 @@
-export const prod = "https://coral-app-3ghm3.ondigitalocean.app";
-// export const prod = "http://localhost:8080";
+// export const prod = "https://coral-app-3ghm3.ondigitalocean.app";
+export const prod = "http://localhost:8080";
 
 export const dev = "http://localhost:8080";
